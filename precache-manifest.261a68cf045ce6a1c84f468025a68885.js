bb54b0500635cd30b7dd3ee188621e8e9b9417ca
Jodi Pranata <jodranata@gmail.com>
2020-02-06 18:25:54 +0700

    Updates

---
+++ precache-manifest.261a68cf045ce6a1c84f468025a68885.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc502b6c977ffa3d110c642e2c0a6ab3",
+    "url": "/FCC-podomoroclock/index.html"
+  },
+  {
+    "revision": "9e9d8f84da8382fbddbe",
+    "url": "/FCC-podomoroclock/static/css/main.fd837532.chunk.css"
+  },
+  {
+    "revision": "99c21d3d01c93fdf2f39",
+    "url": "/FCC-podomoroclock/static/js/2.f5345514.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/FCC-podomoroclock/static/js/2.f5345514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e9d8f84da8382fbddbe",
+    "url": "/FCC-podomoroclock/static/js/main.90471910.chunk.js"
+  },
+  {
+    "revision": "b78144431b1542d5aef4",
+    "url": "/FCC-podomoroclock/static/js/runtime-main.45aa9186.js"
+  }
+]);
